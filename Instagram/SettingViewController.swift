e38d1a2d3c8d4492a58fce75aed76eaba5d2bb08
watomon <watomon1@gmail.com>
2018-02-24 23:41:12 +0900

    Setting.swift完了

--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
+import SVProgressHUD
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var displayNameTextField: UITextField!
+    @IBAction func handleChangeButton(_ sender: Any) {
+    }
+    @IBAction func handleLogoutButton(_ sender: Any) {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 表示名を取得してTextFieldに設定する
+        let user = Auth.auth().currentUser
+        if let user = user {
+            displayNameTextField.text = user.displayName
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
