8d4c1691ee20abc2f6bcfe273b51e30b270dd839
watomon <watomon1@gmail.com>
2018-02-25 01:11:49 +0900

    lv9

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by bitman vam on 2018/02/25.
+//  Copyright © 2018年 watomon. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
