c24c871e49b4b1126e18b2ab49014f67d73f4df9
watomon <watomon1@gmail.com>
2018-02-24 23:26:50 +0900

    Revert "yattimatta"
    This reverts commit 895a22d2ec97b70eb988faedd19fdb57c6d26c72.

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -8,31 +8,8 @@
 
 import UIKit
 
-class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBAction func handleLibraryButton(_ sender: Any) {
-        // ライブラリ（カメラロール）を指定してピッカーを開く
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let pickerController = UIImagePickerController()
-            pickerController.delegate = self
-            pickerController.sourceType = .photoLibrary
-            self.present(pickerController, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func handleCameraButton(_ sender: Any) {
-        // カメラを指定してピッカーを開く
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let pickerController = UIImagePickerController()
-            pickerController.delegate = self
-            pickerController.sourceType = .camera
-            self.present(pickerController, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func handleCancelButton(_ sender: Any) {
-        // 画面を閉じる
-        self.dismiss(animated: true, completion: nil)
-    }
+class ImageSelectViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
